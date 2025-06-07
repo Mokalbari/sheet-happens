813d806d89440b90735592b1b640860dd238464d
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-06-07 17:27:00 +0200

    add i18n

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
+
+export default function HomePage() {
+  const t = useTranslations("HomePage");
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{t("title")}</h1>
+      <Link href="/about">{t("about")}</Link>
     </div>
   );
 }
