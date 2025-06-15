3b8574bda56334f9c3e1abd1dd9802f37ce33bf1
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-06-15 16:47:05 +0200

    remove all tables

--- db/schema/tables.ts
+++
@@ -1,4 +0,0 @@
-export { abilities } from "./tables/abilities";
-export { specieAbilities } from "./tables/specie-abilities";
-export { species } from "./tables/species";
-export { systems } from "./tables/systems";
