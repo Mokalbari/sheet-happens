7e4803d47d5318942b21f39ad2d640cc0df05c07
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-07-09 21:59:16 +0200

    add data for subclasses

---
+++ drizzle/seed/tables/subclasses.ts
@@ -0,0 +1,140 @@
+export const subclassesData = {
+  barbarian: {
+    subclasses: [
+      "Path of the Berserker",
+      "Path of the Wild Heart",
+      "Path of the World Tree",
+      "Path of the Zealot",
+    ],
+    translations: [
+      "Voie du Berserker",
+      "Voie du Cœur Sauvage",
+      "Voie de l’Arbre‑Monde",
+      "Voie du Zélote",
+    ],
+  },
+  bard: {
+    subclasses: [
+      "College of Dance",
+      "College of Glamour",
+      "College of Lore",
+      "College of Valor",
+    ],
+    translations: [
+      "Collège de la Danse",
+      "Collège du Glamour",
+      "Collège du Savoir",
+      "Collège de la Vaillance",
+    ],
+  },
+  cleric: {
+    subclasses: [
+      "Domain of Life",
+      "Domain of Light",
+      "Domain of Trickery",
+      "Domain of War",
+    ],
+    translations: [
+      "Domaine de la Vie",
+      "Domaine de la Lumière",
+      "Domaine de la Tromperie",
+      "Domaine de la Guerre",
+    ],
+  },
+  druid: {
+    subclasses: [
+      "Circle of the Land",
+      "Circle of the Moon",
+      "Circle of the Sea",
+      "Circle of the Stars",
+    ],
+    translations: [
+      "Cercle de la Terre",
+      "Cercle de la Lune",
+      "Cercle de la Mer",
+      "Cercle des Astres",
+    ],
+  },
+  fighter: {
+    subclasses: ["Battle Master", "Champion", "Eldritch Knight", "Psi Warrior"],
+    translations: [
+      "Maître de Guerre",
+      "Champion",
+      "Chevalier Occulte",
+      "Soldat Psi",
+    ],
+  },
+  monk: {
+    subclasses: [
+      "Warrior of the Elements",
+      "Warrior of Mercy",
+      "Warrior of the Open Hand",
+      "Warrior of the Shadow",
+    ],
+    translations: [
+      "Credo des Éléments",
+      "Credo de la Miséricorde",
+      "Credo de la Paume",
+      "Credo de l'Ombre",
+    ],
+  },
+  paladin: {
+    subclasses: [
+      "Oath of the Ancients",
+      "Oath of Devotion",
+      "Oath of Glory",
+      "Oath of Vengeance",
+    ],
+    translations: [
+      "Serment des Anciens",
+      "Serment de Dévotion",
+      "Serment de Gloire",
+      "Serment de Vengeance",
+    ],
+  },
+  ranger: {
+    subclasses: ["Beast Master", "Fey Wanderer", "Gloom Stalker", "Hunter"],
+    translations: [
+      "Belluaire",
+      "Vagabond Féerique",
+      "Traqueur des Ténèbres",
+      "Chasseur",
+    ],
+  },
+  rogue: {
+    subclasses: ["Arcane Trickster", "Assassin", "Soulknife", "Thief"],
+    translations: ["Arnaqueur Arcanique", "Assassin", "Âme Acérée", "Voleur"],
+  },
+  sorcerer: {
+    subclasses: [
+      "Aberrant Sorcery",
+      "Clockwork Sorcery",
+      "Draconic Sorcery",
+      "Wild Magic Sorcery",
+    ],
+    translations: [
+      "Sorcellerie Aberrante",
+      "Sorcellerie Mécanique",
+      "Sorcellerie Draconique",
+      "Sorcellerie Sauvage",
+    ],
+  },
+  warlock: {
+    subclasses: [
+      "Archfey Patron",
+      "Celestial Patron",
+      "Fiend Patron",
+      "Great Old One Patron",
+    ],
+    translations: [
+      "Protecteur Archfée",
+      "Protecteur Céleste",
+      "Protecteur Diabolique",
+      "Protecteur Grand Ancien",
+    ],
+  },
+  wizard: {
+    subclasses: ["Abjurer", "Diviner", "Evoker", "Illusionist"],
+    translations: ["Abjurateur", "Devin", "Évocateur", "Illusionniste"],
+  },
+};
