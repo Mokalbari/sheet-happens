c3b538356cd7394a128985eacb9aaeed4fdfe59a
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-07-20 17:00:17 +0200

    fix wrapper size

--- src/components/ui/wrapper.tsx
+++ src/components/ui/wrapper.tsx
@@ -10,7 +10,7 @@ export function PageWrapper({ children, className, ...props }: Props) {
   return (
     <main
       className={cn(
-        "min-h-screen w-full max-w-4xl mx-auto px-4 relative",
+        "min-h-screen w-full max-w-6xl mx-auto px-4 relative",
         className
       )}
       {...props}
